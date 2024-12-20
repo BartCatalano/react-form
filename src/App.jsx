1d282bf20100e6a3840e9980096739a3eb9e7357
BartCatalano <catalanobartolomeo@gmail.com>
2024-12-20 12:50:13 +0100

    feat: funzionalità eseguita. tasto delete(WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,21 @@ import { useState } from "react";
 
 function App() {
 const  [lista, setLista] = useState ([]);
+const [articoloName, setArticoloName ] = useState ("");
+
+const handArticleForm = (event) => {
+  event.preventDefault ();
+
+  const newArticle = {
+    id: Date.now(),
+    title : articoloName
+  }
+  const newArray = [...lista, newArticle];
+  
+  
+  setLista (newArray );
+  
+}
 
   return (
     <>
@@ -10,26 +25,35 @@ const  [lista, setLista] = useState ([]);
         <section>
  <h2>Articoli inseriti</h2>
  <div className="row row-cols-2 rowcols-lg-3">
- {lista.length === 0 ? (
-            <p>Nessun articolo presente</p>
-          ) : (
-    lista.map((curItem) =>(
+ {lista.length > 0 ? (
+             lista.map((curItem) =>(
         <div className="col" key={curItem.id}>
           <div className="card">
             <div className="card-body">
               <h4>{curItem.title}</h4>
-              <p>{curItem.description}</p>
             </div>
           </div>
         </div>
       ))
+          ) : (<p>Nessun articolo presente</p>
+   
       
           )}
   </div>
   </section>
   <section>
     <h3>aggiungi articolo</h3>
-    <form action=""></form>
+    <form onSubmit={handArticleForm} >
+      <div className="mb-3">
+        <label htmlFor="articoloName">Nome articolo</label>
+        <input type="text" 
+        className="form-control" 
+        id="articoloName" 
+        value={articoloName} 
+        onChange={(event)=> setArticoloName(event.target.value)}/> 
+      </div>
+      <button type="submit" className="btn btn-primary">salva</button>
+    </form>
   </section>
  </div>
     </>
