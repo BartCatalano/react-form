4a61fd6023c3fbbc38c4a46cfa3f6ab438af741a
BartCatalano <catalanobartolomeo@gmail.com>
2024-12-20 15:11:08 +0100

    feat: esercizio con bonus fatto

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,31 @@ import { useState } from "react";
 function App() {
 const  [lista, setLista] = useState ([]);
 const [articoloName, setArticoloName ] = useState ("");
+const [articoloDescription, setArticoloDescription] = useState("");
 
 const handArticleForm = (event) => {
   event.preventDefault ();
 
   const newArticle = {
     id: Date.now(),
-    title : articoloName
+    title : articoloName,
+    description: articoloDescription
   }
   const newArray = [...lista, newArticle];
   
   
   setLista (newArray );
-  
+  setArticoloName("");
+  setArticoloDescription("")
+
 }
+
+const cancella = (idDaCancella) => {
+  const newArray = lista.filter(curArticolo => curArticolo.id !== idDaCancella);
+
+setLista(newArray)
+}
+
 
   return (
     <>
@@ -31,6 +42,8 @@ const handArticleForm = (event) => {
           <div className="card">
             <div className="card-body">
               <h4>{curItem.title}</h4>
+              <p>{curItem.description}</p>
+              <button onClick={()=> cancella(curItem.id)} className="btn btn-danger">cancella</button>
             </div>
           </div>
         </div>
@@ -51,6 +64,14 @@ const handArticleForm = (event) => {
         id="articoloName" 
         value={articoloName} 
         onChange={(event)=> setArticoloName(event.target.value)}/> 
+      </div>
+      <div>
+      <label htmlFor="articoloDescription">Descrizione</label>
+        <input type="text" 
+        className="form-control" 
+        id="articoloDescription" 
+        value={articoloDescription} 
+        onChange={(event)=> setArticoloDescription(event.target.value)}/> 
       </div>
       <button type="submit" className="btn btn-primary">salva</button>
     </form>
