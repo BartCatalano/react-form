8cf5b64bdb5cd3b5bd705fdbb67d3843e7ac21df
BartCatalano <catalanobartolomeo@gmail.com>
2024-12-20 11:50:28 +0100

    feat: generatore articolo dinamico fatto. form(WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
+ 
 
 function App() {
-
+const  [lista, setLista] = useState ([]);
 
   return (
     <>
+      <div className="container">
+        <section>
+ <h2>Articoli inseriti</h2>
+ <div className="row row-cols-2 rowcols-lg-3">
+ {lista.length === 0 ? (
+            <p>Nessun articolo presente</p>
+          ) : (
+    lista.map((curItem) =>(
+        <div className="col" key={curItem.id}>
+          <div className="card">
+            <div className="card-body">
+              <h4>{curItem.title}</h4>
+              <p>{curItem.description}</p>
+            </div>
+          </div>
+        </div>
+      ))
       
- <h1></h1>
+          )}
+  </div>
+  </section>
+  <section>
+    <h3>aggiungi articolo</h3>
+    <form action=""></form>
+  </section>
+ </div>
     </>
-  )
+  );
 }
 
 export default App
